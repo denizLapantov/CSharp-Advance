fce50a1ed76fc6a36cc9bb7e6e9ea0f82f4576c6
Deniz <denizlapantov@gmail.com>
2016-06-09 16:35:11 +0300

    Add files via upload

---
+++ LINQ/SortStudents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.SortStudents
+{
+    public class SortStudents
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<string> myList = new List<string>();
+
+            while (input != "END")
+            {
+                myList.Add(input);
+
+                input = Console.ReadLine();
+            }
+
+            var result = myList.Select(x=>x.Split()).OrderBy(x=>x[1]).ThenByDescending(x=>x[0]);
+            foreach (var name in result)
+            {
+                Console.WriteLine(name[0] + " " + name[1]);
+            }
+
+        }
+    }
+}
---
+++ LINQ/StudentsByAge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.StudentsByAge
+{
+    public class StudentsByAge
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<string> myList = new List<string>();
+
+            while (input != "END")
+            {
+                myList.Add(input);
+                input = Console.ReadLine();
+            }
+
+            var result =
+                myList.Select(x => x.Split()).Where(x => int.Parse(x[2]) >= 18 && int.Parse(x[2]) <= 24).ToList();
+
+            foreach (var name  in result)
+            {
+                Console.WriteLine(name[0] + " " + name[1] + " " + name[2]);
+            }
+        }
+    }
+}
