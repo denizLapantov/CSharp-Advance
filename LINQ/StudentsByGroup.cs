42714e2fd8ef55ca023c45c2227d5b42f0279c24
Deniz <denizlapantov@gmail.com>
2016-06-09 16:31:35 +0300

    Add files via upload

---
+++ LINQ/StudentsByGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.StudentsByGroup
+{
+    public class StudentsByGroup
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<string> myList = new List<string>();
+
+            while (input!="END")
+            {
+               myList.Add(input);
+              
+               input = Console.ReadLine();
+            }
+
+           var result = myList.Select(x => x.Split()).Where(n=>n[2] == "2").OrderBy(x=>x[0]);
+
+            foreach (var name in result)
+            {
+                Console.WriteLine(name[0] + " " + name[1]);
+            }
+            
+        }
+    }
+}
